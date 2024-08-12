7abddb64ad2d9d22402974dcab1dfea312bb0b24
Leonardo Folle <137785767+iLeonard0@users.noreply.github.com>
2024-08-12 20:15:44 -0300

    Create Main.kt

---
+++ Main.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>){
+
+    /*
+    Variaveis Mutaveis
+    - Para isso uso a palavra chave var.
+    */
+
+    var idade = 30
+    idade = 31
+
+    var nome : String = "Leonardo"
+    var valor : Double = 5.5
+    var peso : Int = 90
+    var ePessoa : Boolean = true
+
+
+    println(idade)
+
+
+}
